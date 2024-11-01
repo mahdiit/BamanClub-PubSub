cf0809ba07207438cbdd728272de73ea32a8aa44
Mahdi Yousefi <67848+mahdiit@users.noreply.github.com>
2024-11-01 09:50:15 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace BamanClub_PubSub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            var pubSub = new PubSubHandler();
+            pubSub.Subscribe("topic1",  async (object data) =>
+            {
+                await Task.Delay(1000);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}\tTopic 1 Sub1\t{data.ToString()}");
+                
+            });
+
+            pubSub.Subscribe("topic2", async (object data) =>
+            {
+                await Task.Delay(3000);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}\tTopic 2 Sub 1\t{data.ToString()}");
+                
+            });
+
+
+            pubSub.Subscribe("topic1", async (object data) =>
+            {
+                await Task.Delay(5000);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}\tTopic 1 Sub 2\t{data.ToString()}");
+                
+            });
+
+            pubSub.Subscribe("topic2", async (object data) =>
+            {
+                await Task.Delay(4500);
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}\tTopic2 Sub 2\t{data.ToString()}");
+                
+            });
+
+
+            pubSub.Publish("topic1", "Hello 1");
+            pubSub.Publish("topic2", "Hello 2");
+
+
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
